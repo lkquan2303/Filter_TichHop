084bcfb7fc96056e682b9be09ee946a6fdc88feb
lkquan2303 <55189723+lkquan2303@users.noreply.github.com>
2019-12-10 11:02:58 +0700

    Done All

---
+++ app/src/main/java/info/androidhive/imagefilters/EditImageFragment.java
@@ -0,0 +1,113 @@
+package info.androidhive.imagefilters;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class EditImageFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
+
+    private EditImageFragmentListener listener;
+
+    @BindView(R.id.seekbar_brightness)
+    SeekBar seekBarBrightness;
+
+    @BindView(R.id.seekbar_contrast)
+    SeekBar seekBarContrast;
+
+    @BindView(R.id.seekbar_saturation)
+    SeekBar seekBarSaturation;
+
+    public void setListener(EditImageFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    public EditImageFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_image, container, false);
+
+        ButterKnife.bind(this, view);
+
+
+        seekBarBrightness.setMax(200);
+        seekBarBrightness.setProgress(100);
+        seekBarContrast.setMax(20);
+        seekBarContrast.setProgress(0);
+        seekBarSaturation.setMax(30);
+        seekBarSaturation.setProgress(10);
+
+        seekBarBrightness.setOnSeekBarChangeListener(this);
+        seekBarContrast.setOnSeekBarChangeListener(this);
+        seekBarSaturation.setOnSeekBarChangeListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+        if (listener != null) {
+
+            if (seekBar.getId() == R.id.seekbar_brightness) {
+                listener.onBrightnessChanged(progress - 100);
+            }
+
+            if (seekBar.getId() == R.id.seekbar_contrast) {
+                progress += 10;
+                float floatVal = .10f * progress;
+                listener.onContrastChanged(floatVal);
+            }
+
+            if (seekBar.getId() == R.id.seekbar_saturation) {
+                float floatVal = .10f * progress;
+                listener.onSaturationChanged(floatVal);
+            }
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        if (listener != null)
+            listener.onEditStarted();
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (listener != null)
+            listener.onEditCompleted();
+    }
+
+    public void resetControls() {
+        seekBarBrightness.setProgress(100);
+        seekBarContrast.setProgress(0);
+        seekBarSaturation.setProgress(10);
+    }
+
+    public interface EditImageFragmentListener {
+        void onBrightnessChanged(int brightness);
+
+        void onSaturationChanged(float saturation);
+
+        void onContrastChanged(float contrast);
+
+        void onEditStarted();
+
+        void onEditCompleted();
+    }
+}
